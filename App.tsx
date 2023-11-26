4dd6326b71685622c2436e46fcf372f428b183a6
guodevguru <guobrian118@gmail.com>
2023-11-26 12:26:57 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { View } from 'react-native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <View />
+    </NavigationContainer>
+  );
+};
+
+export default App;
